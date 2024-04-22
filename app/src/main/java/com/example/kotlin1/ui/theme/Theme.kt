4586d39885b0e5af315567c3e3ab82191c2fc228
gorkijatt <gorkijaxy@gmail.com>
2024-04-22 17:17:46 +0530

    Tranparent Status Bar

--- app/src/main/java/com/example/kotlin1/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kotlin1/ui/theme/Theme.kt
@@ -54,11 +54,19 @@ fun Kotlin1Theme(
         else -> LightColorScheme
     }
     val view = LocalView.current
+//    if (!view.isInEditMode) {
+//        SideEffect {
+//            val window = (view.context as Activity).window
+//            window.statusBarColor = colorScheme.primary.toArgb()
+//            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+//        }
+//    }
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
